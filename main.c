b7b33f64002b7ad68e13b0e3ed49eef736188c21
Katarína Grešová <katarinkagres@gmail.com>
2018-06-19 22:18:51 +0200

    help function

--- main.c
+++ main.c
@@ -1,4 +1,18 @@
 #include "main.h"
-int main() {
-    return 0;
+
+int main(int argc, char* argv[]) {
+
+    if (argc > 1) {
+        help();
+        return 0;
+    }
+}
+
+void help() {
+    printf("\n"
+           "Simple text processing.\n"
+           "Usage: ./text-processing\n"
+           "Functionality: program reads data from stdin word by word. Each word is processed and result is printed to stdout.\n"
+           "Words are searched for prime number, correct date and palindrome.\n"
+           "\n");
 }
--- main.h
+++ main.h
@@ -1,4 +1,8 @@
 #ifndef TEXT_PROCESSING_MAIN_H
 #define TEXT_PROCESSING_MAIN_H
 
+#include <stdio.h>
+
+void help();
+
 #endif //TEXT_PROCESSING_MAIN_H
